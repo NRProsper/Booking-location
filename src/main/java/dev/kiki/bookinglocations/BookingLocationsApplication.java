dcee6b296ed4f0fe020826bbbac8d5e23c0e1b29
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-03 15:32:25 +0200

    chore: initial commit

---
+++ src/main/java/dev/kiki/bookinglocations/BookingLocationsApplication.java
@@ -0,0 +1,13 @@
+package dev.kiki.bookinglocations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingLocationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingLocationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/kiki/bookinglocations/BookingLocationsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kiki.bookinglocations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingLocationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
